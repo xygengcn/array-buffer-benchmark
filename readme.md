527f845765946e49b6dcbf8abf88681491f19e2d
xygengcn <xygengcn@qq.com>
2023-06-05 22:59:57 +0800

    fix: 修正函数

--- readme.md
+++ readme.md
@@ -1,21 +1,23 @@
 # array-buffer 相互转换的性能对比
 
+> 测试机器：Apple M2 16GB
+
 ## 内容
 
 ```
 array to buffer 结果
 
 
-cycle:  jsonToBuffer x 101,884 ops/sec ±0.65% (93 runs sampled)
-cycle:  ampToBuffer x 158,188 ops/sec ±12.40% (93 runs sampled)
-cycle:  packToBuffer x 331,227 ops/sec ±0.72% (90 runs sampled)
+cycle:  jsonToBuffer x 98,358 ops/sec ±1.55% (96 runs sampled)
+cycle:  ampToBuffer x 163,682 ops/sec ±12.60% (95 runs sampled)
+cycle:  packToBuffer x 341,155 ops/sec ±0.25% (100 runs sampled)
 Fastest is packToBuffer
 
 buffer to array 结果
 
-cycle:  buffertoJson x 44,054 ops/sec ±0.41% (91 runs sampled)
-cycle:  ampToJson x 50,576 ops/sec ±0.85% (85 runs sampled)
-cycle:  unpackToJson x 333,039 ops/sec ±0.84% (92 runs sampled)
+cycle:  buffertoJson x 43,383 ops/sec ±0.52% (97 runs sampled)
+cycle:  ampToJson x 50,593 ops/sec ±0.75% (90 runs sampled)
+cycle:  unpackToJson x 51,816 ops/sec ±0.58% (91 runs sampled)
 Fastest is unpackToJson
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import AmpMessage from 'amp-message';
-import { pack } from 'msgpackr';
+import { pack, unpack } from 'msgpackr';
 import Benchmark from 'benchmark';
 const arr = [
     '西班牙曾经提到过，自己的鞋子，自己知道紧在哪里。这句话语虽然很短，但令我浮想联翩。 每个人都不得不面对这些问题。 在面对这种问题时， 卡耐基曾经提到过，一个不注意小事情的人，永远不会成就大事业。带着这句话，我们还要更加慎重的审视这个问题： 既然如何， 对我个人而言，科学和人文谁更有意义不仅仅是一个重大的事件，还可能会改变我的人生。 带着这些问题，我们来审视一下科学和人文谁更有意义。 既然如此， 科学和人文谁更有意义，发生了会如何，不发生又会如何。 生活中，若科学和人文谁更有意义出现了，我们就不得不考虑它出现了的事实。 我们都知道，只要有意义，那么就必须慎重考虑。 可是，即使是这样，科学和人文谁更有意义的出现仍然代表了一定的意义。 科学和人文谁更有意义因何而发生？ 本人也是经过了深思熟虑，在每个日日夜夜思考这个问题。 就我个人来说，科学和人文谁更有意义对我的意义，不能不说非常重大。 可是，即使是这样，科学和人文谁更有意义的出现仍然代表了一定的意义。 所谓科学和人文谁更有意义，关键是科学和人文谁更有意义需要如何写。',
@@ -11,6 +11,8 @@ const arr = [
 const buf = jsonToBuffer();
 
 const buf2 = ampToBuffer();
+
+const buf3 = packToBuffer();
 
 /**
  * 原生
@@ -45,7 +47,7 @@ function packToBuffer() {
     return pack(arr);
 }
 function unpackToJson() {
-    return pack(arr);
+    return unpack(buf3);
 }
 
 const suite1 = new Benchmark.Suite('jsonToBuffer');
